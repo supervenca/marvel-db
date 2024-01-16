5f3583bc1b771c3cf3fc275510f2533575afa7b5
Ventseslava Shopova <supervenca@yandex.ru>
2024-01-16 21:36:51 +0200

    routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,49 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PropTypes from 'prop-types';
 
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-    
-
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
-
         return (
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={onCharSelected}/>
-                        </ErrorBoundary>
-                        
-                        <ErrorBoundary>
-                            <CharInfo charId={selectedChar}/>
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </main>
-                <main>
-                    <ComicsList/>
-                </main>
-            </div>
+            <Router>
+                <div className="app">
+                    <AppHeader/>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
         )
-    
-    
 }
 
 App.propTypes = {
